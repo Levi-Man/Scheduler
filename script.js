8add2e0cfcbab9f9592a54006fa2336657c5993c
Indra Levi Manahan <indra.levi.manahan@gmail.com>
2023-11-23 16:43:30 -0500

    fixed logic of saving to and getting from local storage and then rendering to the page

--- script.js
+++ script.js
@@ -5,75 +5,87 @@
 const maxHour = 17;
 const minHour = 9;
 
-$(function () {
+$(document).ready(function () {
 
-   let buttonSubmit = document.getElementsByClassName("btn")
-//saving the input to local storage
-  for (i = 0; i < buttonSubmit.length; i++) {
-    buttonSubmit[i].addEventListener("click", function (e) { 
-      e.preventDefault();
-      console.log(e.target.parentElement.previousElementSibling)
-      let taskInput = e.target.parentElement.previousElementSibling.value;
-      // document.querySelector('.textInput').value;
-      localStorage.setItem("task " + this.id, taskInput);
-      console.log("test", taskInput);
-      console.log("button clicked", this.id)
-      console.log(e.target);
-      console.log(e.target.previousElementSibling);
+  
+  //saving the input to local storage
+  
+  $(".saveBtn").on("click", function (e) {
 
-      let savedTask = localStorage.getItem("task" + this.id);
-      document.querySelector(".textInput").value = savedTask;
-      console.log(savedTask);
-      
-      let renderTask = function() { $(e.target.parentElement.previousElementSibling).text(savedTask);
-      }
+    // console.log(e.target.parentElement.previousElementSibling)
+    let taskInput = e.target.parentElement.previousElementSibling.value;
+    // document.querySelector('.textInput').value;
+    localStorage.setItem("task " + this.id, taskInput);
+    console.log("test", taskInput);
+    console.log("button clicked", this.id)
+    console.log(e.target);
+    console.log(e.target.previousElementSibling);
 
-      renderTask();
+    // let savedTask = localStorage.getItem("task" + this.id);
+    // document.querySelector(".textInput").value = savedTask;
+    // console.log(savedTask);
+
+    // let renderTask = function () {
+    //   $(e.target.parentElement.previousElementSibling).text(savedTask);
+    // }
+
+    // renderTask();
     //   localStorage.getItem("task");
     //   $(e.target.parentElement.previousElementSibling).text(renderTask);
     // console.log(renderTask);
-    })
-  }
+  })
+
+
+//getting value from local storage and putting into respective text boxes
+  $("#9 .description").val(localStorage.getItem("task 9"));
+  $("#10 .description").val(localStorage.getItem("task 10"));
+  $("#11 .description").val(localStorage.getItem("task 11"));
+  $("#12 .description").val(localStorage.getItem("task 12"));
+  $("#13 .description").val(localStorage.getItem("task 13"));
+  $("#14 .description").val(localStorage.getItem("task 14"));
+  $("#15 .description").val(localStorage.getItem("task 15"));
+  $("#16 .description").val(localStorage.getItem("task 16"));
+  $("#17 .description").val(localStorage.getItem("task 17"));
 
 // //rendering saved info on page
-  // localStorage.getItem("task");
+// localStorage.getItem("task");
 
-  // let renderTask = function() {
-  //   $(e.target.parentElement.previousElementSibling).text(renderTask);
-  //   console.log(renderTask);
-  // }
+// let renderTask = function() {
+//   $(e.target.parentElement.previousElementSibling).text(renderTask);
+//   console.log(renderTask);
+// }
 
 //   renderTask();
 
-  console.log("hello" + document.querySelector(".textInput").id);
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+console.log("hello" + document.querySelector(".textInput").id);
+// TODO: Add a listener for click events on the save button. This code should
+// use the id in the containing time-block as a key to save the user input in
+// local storage. HINT: What does `this` reference in the click listener
+// function? How can DOM traversal be used to get the "hour-x" id of the
+// time-block containing the button that was clicked? How might the id be
+// useful when saving the description in local storage?
 
 
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  let dayDate = setInterval(function () {
-    let reformatDate = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a');
-    $('#currentDay').text(reformatDate);
-  }
-    , 1000)
-});
-
+//
+// TODO: Add code to apply the past, present, or future class to each time
+// block by comparing the id to the current hour. HINTS: How can the id
+// attribute of each time-block be used to conditionally add or remove the
+// past, present, and future classes? How can Day.js be used to get the
+// current hour in 24-hour time?
+//
+// TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id
+// attribute of each time-block be used to do this?
+//
+// TODO: Add code to display the current date in the header of the page.
 let dayDate = setInterval(function () {
+  let reformatDate = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a');
+  $('#currentDay').text(reformatDate);
+}
+  , 1000)
+;
+
+let dayHour = setInterval(function () {
   let currentHour = dayjs().hour();;
   console.log("current hour" + currentHour);
 }
@@ -103,8 +115,8 @@ if (currentHour <= maxHour) {
     $(document.getElementById(i)).css('background-color', '').css('background-color', 'beige');
   }
   console.log("past office hours");
-};
-
+}
+});
 
 
 
