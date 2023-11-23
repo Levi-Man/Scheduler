353033b29a5785a3feec41d6c56742218ad98a3f
Indra Levi Manahan <indra.levi.manahan@gmail.com>
2023-11-23 17:24:10 -0500

    fixed error in pre-office hours rendering

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const minHour = 9;
 
 $(document).ready(function () {
 
-  
+
   //saving the input to local storage
-  
+
   $(".saveBtn").on("click", function (e) {
 
     // console.log(e.target.parentElement.previousElementSibling)
@@ -36,7 +36,7 @@ $(document).ready(function () {
   })
 
 
-//getting value from local storage and putting into respective text boxes
+  //getting value from local storage and putting into respective text boxes
   $("#9 .description").val(localStorage.getItem("task 9"));
   $("#10 .description").val(localStorage.getItem("task 10"));
   $("#11 .description").val(localStorage.getItem("task 11"));
@@ -47,75 +47,76 @@ $(document).ready(function () {
   $("#16 .description").val(localStorage.getItem("task 16"));
   $("#17 .description").val(localStorage.getItem("task 17"));
 
-// //rendering saved info on page
-// localStorage.getItem("task");
+  // //rendering saved info on page
+  // localStorage.getItem("task");
 
-// let renderTask = function() {
-//   $(e.target.parentElement.previousElementSibling).text(renderTask);
-//   console.log(renderTask);
-// }
+  // let renderTask = function() {
+  //   $(e.target.parentElement.previousElementSibling).text(renderTask);
+  //   console.log(renderTask);
+  // }
 
-//   renderTask();
+  //   renderTask();
 
-console.log("hello" + document.querySelector(".textInput").id);
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-
-
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO: Add code to display the current date in the header of the page.
-let dayDate = setInterval(function () {
-  let reformatDate = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a');
-  $('#currentDay').text(reformatDate);
-}
-  , 1000)
-;
-
-let dayHour = setInterval(function () {
-  let currentHour = dayjs().hour();;
-  console.log("current hour" + currentHour);
-}
-  , 10000)
-
-// Get the current hour
-const currentHour = new Date().getHours();
-// let currentHour = 14;
+  console.log("hello" + document.querySelector(".textInput").id);
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
 
 
-// Get the element you want to change the class of
-const element = document.querySelector(".time-block");
-
-// Change class based on the current hour. If outside office hours, beige background
-
-if (currentHour <= maxHour) {
-  for (let i = 9; i < currentHour; i++) {
-    document.getElementById(i).classList.add("past");
+  //
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+  //
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  //
+  // TODO: Add code to display the current date in the header of the page.
+  let dayDate = setInterval(function () {
+    let reformatDate = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a');
+    $('#currentDay').text(reformatDate);
   }
-  document.getElementById(currentHour).classList.add("present");
-  for (let i = currentHour + 1; i <= maxHour; i++) {
-    document.getElementById(i).classList.add("future");
-  }
-} else {
-  for (let i = 9; i < currentHour; i++) {
+    , 1000)
+    ;
 
-    $(document.getElementById(i)).css('background-color', '').css('background-color', 'beige');
+  let dayHour = setInterval(function () {
+    let currentHour = dayjs().hour();;
+    console.log("current hour" + currentHour);
   }
-  console.log("past office hours");
-}
+    , 10000)
+
+  // Get the current hour
+  // const currentHour = new Date().getHours();
+  let currentHour = 8;
+
+
+  // Get the element you want to change the class of
+  const element = document.querySelector(".time-block");
+
+  // Change class based on the current hour. If outside office hours, beige background
+
+  if (currentHour <= maxHour && currentHour >= minHour) {
+    for (let i = minHour; i < currentHour; i++) {
+      document.getElementById(i).classList.add("past");
+    }
+    document.getElementById(currentHour).classList.add("present");
+    for (let i = currentHour + 1; i <= maxHour; i++) {
+      document.getElementById(i).classList.add("future");
+    }
+  } else {
+    for (let i = minHour; i <= maxHour; i++) {
+      console.log("this", i);
+      $(document.getElementById(i)).css('background-color', '').css('background-color', 'beige');
+      
+    }
+    console.log("past office hours");
+  } 
 });
 
 
